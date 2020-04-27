d4968f9d47034d137bebc95bf66da219c272dd98
Response777 <CoolResponse777@gmail.com>
2020-04-27 15:10:22 +0200

    Reorg.

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Author: Tianjian Jiang <jiangtj13@gmail.com>
-
-from kits import flags
-from kits import gpu
-from kits.xmlparser import xml2dict
---
+++ kits/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Author: Response777 <CoolResponse777@gmail.com>
+
+from kits.utils import flags
